d193ba73f3a5bef50fa38b6c3077e344a0e268ec
Mehdi kordani <mehdi@Mehdis-MacBook-Air.local>
2024-07-22 17:42:56 +0330

    fixing lint errors

--- script.py
+++ script.py
@@ -1,6 +1,15 @@
-# script.py
+"""
+This script performs [describe the purpose of your script here].
+It includes the following functionalities:
+1. [Describe functionality 1]
+2. [Describe functionality 2]
+...
+"""
 
 def main():
+    """
+    code main function
+    """
     print("Hello, GitHub Actions!")
 
 if __name__ == "__main__":
